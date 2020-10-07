23d520973c8964d301bcda97a44933d0cd1547de
InveterateCoder <inveterate.coder@gmail.com>
2020-10-07 23:01:22 +0300

    wth

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import AppBar from './AppBar.jsx'
 import Drawer from './Drawer.jsx'
 import Main from './Main.jsx'
+import Login from './Login.jsx'
+import keys from './keys'
 
 export default function Home() {
-  return (
-    <>
-      <AppBar />
-      <Drawer />
-      <Main />
-    </>
-  )
+  if (keys.token) {
+    return (
+      <>
+        <AppBar />
+        <Drawer />
+        <Main />
+      </>
+    )
+  }
+  return <Login />
 }
